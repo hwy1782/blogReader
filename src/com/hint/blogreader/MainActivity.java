6180cbf9d1ffcddf015d85185159e63c1da17cec
hint honge <hwy1782@gmail.com>
2013-03-04 15:54:57 +0800

    modify welcome page textview

--- src/com/hint/blogreader/MainActivity.java
+++ src/com/hint/blogreader/MainActivity.java
@@ -1,15 +1,25 @@
 package com.hint.blogreader;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private static final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	
+	private TextView textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		textView = (TextView)findViewById(R.id.welcomeId);
+		textView.setText("现在时间： "+df.format(new Date()));
 	}
 
 	@Override
